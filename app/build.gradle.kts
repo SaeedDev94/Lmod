787ff3d40f5311754ca0026fcf1a5146604742cc
SaeedDev94 <saeedp47@gmail.com>
2024-10-23 23:13:03 +0330

    OtpExtractor: added more patterns

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "io.github.saeeddev94.lmod"
         minSdk = 26
         targetSdk = 34
-        versionCode = 10
-        versionName = "2.0.0"
+        versionCode = 14
+        versionName = "2.1.0"
 
         vectorDrawables {
             useSupportLibrary = true
--- app/src/main/java/io/github/saeeddev94/lmod/OtpExtractor.kt
+++ app/src/main/java/io/github/saeeddev94/lmod/OtpExtractor.kt
@@ -3,14 +3,23 @@ package io.github.saeeddev94.lmod
 class OtpExtractor {
     companion object {
         fun extract(text: String): String? {
-            val webOtp = "(?i)(?:#|code:\\s*)(\\d{4,8})".toRegex()
-            val bankOtp = "(?i)(رمز\\s+پویا)(\\s*)(\\d{4,8})".toRegex()
-            return match(text, webOtp) ?: match(text, bankOtp, 3)
+            return match(text, listOf("code")) ?:
+              match(text, listOf("رمز", "پویا")) ?:
+              match(text, listOf("عدد", "محرمانه")) ?:
+              match(text, listOf("کد", "ورود")) ?:
+              match(text, listOf("کد", "تایید", "شما"))
         }
 
-        private fun match(text: String, regex: Regex, group: Int = 1): String? {
+        private fun regex(list: List<String>): Regex {
+            val words = list.joinToString("\\s+")
+            val pattern = "(?i)(#|{WORDS}\\s*:?)(\\s*)(\\d{4,8})"
+            return pattern.replace("{WORDS}", words).toRegex()
+        }
+
+        private fun match(text: String, list: List<String>): String? {
+            val regex = regex(list)
             val matchResult = regex.find(text)
-            return matchResult?.groups?.get(group)?.value
+            return matchResult?.groups?.get(3)?.value
         }
     }
 }
