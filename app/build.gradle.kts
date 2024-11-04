6640733ac08cb582cbdcbef14cfda5a91543685c
SaeedDev94 <saeedp47@gmail.com>
2024-11-04 21:35:25 +0330

    v2.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "io.github.saeeddev94.lmod"
         minSdk = 26
         targetSdk = 35
-        versionCode = 14
-        versionName = "2.1.0"
+        versionCode = 16
+        versionName = "2.2.0"
 
         vectorDrawables {
             useSupportLibrary = true
