944295393ef4ff64153259efb01bd97c8f9b60f0
SaeedDev94 <saeedp47@gmail.com>
2024-12-17 11:27:00 -0700

    Jelly: Fix popup issue

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "io.github.saeeddev94.lmod"
         minSdk = 26
         targetSdk = 35
-        versionCode = 24
+        versionCode = 27
         versionName = "2.6.0"
 
         vectorDrawables {
--- app/src/main/java/io/github/saeeddev94/lmod/HookEntry.kt
+++ app/src/main/java/io/github/saeeddev94/lmod/HookEntry.kt
@@ -2,7 +2,9 @@ package io.github.saeeddev94.lmod
 
 import android.content.Context
 import android.content.Intent
+import android.os.Message
 import android.provider.Telephony
+import android.webkit.WebView
 import com.highcapable.yukihookapi.annotation.xposed.InjectYukiHookWithXposed
 import com.highcapable.yukihookapi.hook.factory.configs
 import com.highcapable.yukihookapi.hook.factory.encase
@@ -58,6 +60,30 @@ class HookEntry : IYukiHookXposedInit {
                     after {
                         val webSettings = result as android.webkit.WebSettings
                         webSettings.userAgentString = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
+                    }
+                }
+            }
+
+            "org.lineageos.jelly.webview.ChromeClient".toClassOrNull()?.apply {
+                method {
+                    name = "onCreateWindow"
+                    param(WebView::class.java, Boolean::class.java, Boolean::class.java, Message::class.java)
+                }.hook {
+                    replaceAny {
+                        val ref = instance::class.java
+                        val activityField = ref.getDeclaredField("activity")
+                        val incognitoField = ref.getDeclaredField("incognito")
+                        activityField.isAccessible = true
+                        incognitoField.isAccessible = true
+                        val activity = activityField.get(instance) as Context
+                        val incognito = incognitoField.get(instance) as Boolean
+
+                        val view = args[0] as WebView
+                        val isDialog = args[1] as Boolean
+                        val isUserGesture = args[2] as Boolean
+                        val resultMsg = args[3] as Message
+
+                        ChromeClient(activity, incognito).onCreateWindow(view, isDialog, isUserGesture, resultMsg)
                     }
                 }
             }
